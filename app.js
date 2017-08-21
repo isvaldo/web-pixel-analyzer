f27f9387b90b5bde071e89b6c13ee178b66ff7e5
Isvaldo Souza <isvaldo.fernandes@gmail.com>
2017-08-20 21:50:51 -0300

    função que retorna uma porção de pixels pela posição do mouse

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var restify = require('restify');
+var robot = require("robotjs");
 const fs = require('fs');
 
 function pixelWebView(req, res, next) {
@@ -9,6 +10,7 @@ function pixelWebView(req, res, next) {
             return;
         }
         res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Access-Control-Allow-Origin', '"*"');
         res.writeHead(200);
         res.end(data);
         next();
@@ -16,7 +18,24 @@ function pixelWebView(req, res, next) {
 
 }
 
+
+
 function pixelData(req, res, next) {
+    var mousePos = robot.getMousePos();
+    var img = robot.screen.capture(mousePos.x, mousePos.y, 10, 10);
+    var response = [];
+
+    for (var i =0;i<img.width;i++){
+        var item = [];
+        for (var j=0;j<img.height;j++){
+            var colorHex = img.colorAt(i, j);
+            item.push(colorHex);
+        }
+        response.push(item);
+    }
+    res.send(response);
+
+
     next();
 }
 
@@ -24,6 +43,7 @@ var server = restify.createServer();
 
 server.get('/', pixelWebView);
 server.get('/data', pixelData);
+
 
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
